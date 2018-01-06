d03ba0613023a7d5e260350c6149c56dce76b605
张成 <zhang5474jj@163.com>
2018-01-06 22:59:28 +0800

    完成dashboard

--- application/index/controller/Dashboard.php
+++ application/index/controller/Dashboard.php
@@ -9,6 +9,10 @@
 namespace app\index\controller;
 
 
+use app\index\model\ProjectUser as ProjectUserModel;
+use app\index\model\Bug as BugModel;
+
+
 class Dashboard extends Auth
 {
 
@@ -20,8 +24,14 @@ class Dashboard extends Auth
     public function index()
     {
 
+        $this->assign('project_count',ProjectUserModel::where(['user_id'=>$this->user_id])->count());
+        $this->assign('bug_count_no',BugModel::where(['current_user_id'=>$this->user_id,'bug_status'=>0])->count());
+        $this->assign('bug_count_yes',BugModel::where(['current_user_id'=>$this->user_id,])->where('bug_status','>',0)->count());
 
+        $projectIds = ProjectUserModel::all(['user_id'=>$this->user_id]);
+        $projectIdArray = array_column($projectIds->toArray(),'project_id');
 
+        $this->assign('bug_count_all',BugModel::where(['current_user_id'=>$this->user_id,])->where('project_id','in',$projectIdArray)->count());
 
 
         $this->assign('title',lang('Dashboard'));
