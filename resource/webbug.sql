3bbc183a912e59d6345f0731f24a1863426310f8
phpzc <zhang5474jj@163.com>
2019-11-25 11:21:50 +0800

    添加缺失的 jobs表结构 更新sql文件

--- resource/webbug.sql
+++ resource/webbug.sql
@@ -1,30 +1,30 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : phpzc.net
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50719
- Source Host           : www.phpzc.net
- Source Database       : webbug
+ Source Server Version : 80013
+ Source Host           : localhost:3306
+ Source Schema         : webbug
 
  Target Server Type    : MySQL
- Target Server Version : 50719
- File Encoding         : utf-8
+ Target Server Version : 80013
+ File Encoding         : 65001
 
- Date: 01/14/2018 15:18:08 PM
+ Date: 25/11/2019 09:15:41
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `wb_bug`
+-- Table structure for wb_bug
 -- ----------------------------
 DROP TABLE IF EXISTS `wb_bug`;
 CREATE TABLE `wb_bug` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `bug_title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `bug_content` text COLLATE utf8mb4_unicode_ci,
+  `bug_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bug_content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `create_time` datetime DEFAULT NULL,
   `create_user_id` int(11) DEFAULT NULL,
   `project_id` int(11) DEFAULT NULL,
@@ -35,10 +35,10 @@ CREATE TABLE `wb_bug` (
   `module_id` int(11) DEFAULT NULL COMMENT '模块id',
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='bug表';
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='bug表';
 
 -- ----------------------------
---  Table structure for `wb_bug_log`
+-- Table structure for wb_bug_log
 -- ----------------------------
 DROP TABLE IF EXISTS `wb_bug_log`;
 CREATE TABLE `wb_bug_log` (
@@ -46,7 +46,7 @@ CREATE TABLE `wb_bug_log` (
   `bug_id` bigint(20) DEFAULT NULL,
   `old_user_id` int(11) DEFAULT NULL COMMENT '原BUG所属用户',
   `new_user_id` int(11) DEFAULT NULL COMMENT '分配给的新的用户',
-  `content` text COLLATE utf8mb4_unicode_ci,
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `old_bug_status` tinyint(2) DEFAULT NULL COMMENT '原BUG处理状态',
@@ -55,47 +55,63 @@ CREATE TABLE `wb_bug_log` (
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='bug修改日志';
 
 -- ----------------------------
---  Table structure for `wb_login_log`
+-- Table structure for wb_jobs
+-- ----------------------------
+DROP TABLE IF EXISTS `wb_jobs`;
+CREATE TABLE `wb_jobs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `queue` varchar(255) NOT NULL,
+  `payload` longtext NOT NULL,
+  `attempts` tinyint(3) unsigned NOT NULL,
+  `reserved` tinyint(3) unsigned NOT NULL,
+  `reserved_at` int(10) unsigned DEFAULT NULL,
+  `available_at` int(10) unsigned NOT NULL,
+  `created_at` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='tp队列任务表';
+
+-- ----------------------------
+-- Table structure for wb_login_log
 -- ----------------------------
 DROP TABLE IF EXISTS `wb_login_log`;
 CREATE TABLE `wb_login_log` (
   `id` bigint(20) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `login_date` datetime DEFAULT NULL,
-  `login_ip` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `login_ip` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='登陆记录';
 
 -- ----------------------------
---  Table structure for `wb_project`
+-- Table structure for wb_project
 -- ----------------------------
 DROP TABLE IF EXISTS `wb_project`;
 CREATE TABLE `wb_project` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `project_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `project_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
-  `create_ip` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `create_ip` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='项目表';
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='项目表';
 
 -- ----------------------------
---  Table structure for `wb_project_module`
+-- Table structure for wb_project_module
 -- ----------------------------
 DROP TABLE IF EXISTS `wb_project_module`;
 CREATE TABLE `wb_project_module` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(11) DEFAULT NULL,
-  `module_name` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `module_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
---  Table structure for `wb_project_user`
+-- Table structure for wb_project_user
 -- ----------------------------
 DROP TABLE IF EXISTS `wb_project_user`;
 CREATE TABLE `wb_project_user` (
@@ -105,38 +121,38 @@ CREATE TABLE `wb_project_user` (
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
---  Table structure for `wb_project_version`
+-- Table structure for wb_project_version
 -- ----------------------------
 DROP TABLE IF EXISTS `wb_project_version`;
 CREATE TABLE `wb_project_version` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(11) DEFAULT NULL,
-  `version_name` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `version_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
---  Table structure for `wb_user`
+-- Table structure for wb_user
 -- ----------------------------
 DROP TABLE IF EXISTS `wb_user`;
 CREATE TABLE `wb_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `github` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `github` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
-  `create_ip` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `create_ip` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email_status` tinyint(1) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
-  `nickname` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '昵称',
+  `nickname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '昵称',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
 
 SET FOREIGN_KEY_CHECKS = 1;
